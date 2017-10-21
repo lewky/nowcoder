cbe8e87496a4b34dd433c5bd8e624ed15fdd7a71
Lewis <1019175915@qq.com>
2017-10-21 12:49:40 +0800

    algorithm learning

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # nowcoder
-It's my algorithm learning process for nowcoder
+It's my algorithm learning repository for nowcoder.
+
+# Links
+
+1. [牛客网](https://www.nowcoder.com)
+
+2. [剑指Offer_编程题](https://www.nowcoder.com/ta/coding-interviews?page=1)
+
+# Contents
+
+## 剑指Offer_编程题
+
+|序号|考点|题目|
+|-:|-|-|
+|1|数组|二维数组中的查找|
---
+++ 剑指Offer_编程题/1/Solution.java
@@ -0,0 +1,50 @@
+/*
+	题目：
+	在一个二维数组中，每一行都按照从左到右递增的顺序排序，
+	每一列都按照从上到下递增的顺序排序。请完成一个函数，
+	输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+*/
+
+/*
+	解题思路：
+	二维数组类似于一个二维矩阵，这样一个规定了排序的二维矩阵，
+	从其左下角开始判断，如果比左下角的数值小就向上移动，反之就向右移动。
+	由于二维数组的第二维长度不一定等长，所以每次都要判断是否会下标越界。
+	比如输入这样一个二维数组，并查找是否存在5或7：
+	{
+		{1,3,5},
+		{2,4}, 
+		{3,6,8,9}
+	}
+*/
+
+public class Solution {
+    public static boolean find(int target, int [][] array) {
+        int i = array.length - 1;
+        int j = 0;
+        int k = 0;
+        while(i >= 0){
+            k = array[i].length;
+            if(j == k){
+                i--;
+                continue;
+            }
+            if(target == array[i][j]){
+                return true;
+            }else if(target > array[i][j]){
+                j++;
+            }else if(target < array[i][j]){
+            	i--;
+            }
+        }
+        return false;
+    }
+
+	public static void main(String[] args){
+		int[][] array = {{1,3,5}, {2,4}, {3,6,8,9}};
+		boolean flag = find(5,array);
+		System.out.println(flag);
+		flag = find(7,array);
+		System.out.println(flag);
+	}
+}
